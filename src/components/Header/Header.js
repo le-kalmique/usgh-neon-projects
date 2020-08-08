d633a710a7ed4578a2e791560d6a8047027a6924
Renata <rdzhergalova@gmail.com>
2020-08-08 13:37:27 +0300

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faNewspaper, faUsers, faFire } from '@fortawesome/free-solid-svg-icons';
+
+import  { getCurrentUser } from '../../utils/auth';
+
+import "./header.css"
+
+function Header() {
+
+  const user = getCurrentUser();
+
+  return (
+    <header className="header">
+      <Link to="/" className="link"><h1 className="header_title">NEON</h1></Link>
+      <nav className="header_nav">
+        <Link to="#" className="link header_link" activeClassName="header_link_active"><FontAwesomeIcon className="header_icon" icon={faNewspaper}/></Link>
+        <Link to="#" className="link header_link" activeClassName="header_link_active"><FontAwesomeIcon className="header_icon" icon={faUsers}/></Link>
+        <Link to="#" className="link header_link" activeClassName="header_link_active"><FontAwesomeIcon className="header_icon" icon={faFire}/></Link>
+      </nav>
+
+      {user ?
+        <Link to="/profile" className="link header_link">
+          <span className="header_profile-name">{}</span>
+          <span className="header_profile-image">{}</span>
+        </Link>
+        :
+        <Link to="/auth/login" className="link header_link header_login">
+          Login
+        </Link>
+      }
+
+    </header>
+  );
+}
+
+
+export default Header;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Header from "../../components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <Header/>
 
-    </div>
+      <Switch>
+        <Route path={"/"}>
+        </Route>
+        <Route path={"/subscriptions"}>
+
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
